f2494778f984873d527e732c4bedee2813c00b2e
Lucas Leocadio de Souza <vanildo1511@gmail.com>
2025-07-24 21:57:14 -0300

    Terminado alert precisa rever todas as aplicacoes ainda

--- src/main/java/com/cestec/cestec/service/comWindowService.java
+++ src/main/java/com/cestec/cestec/service/comWindowService.java
@@ -7,7 +7,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.stream.Collectors;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.pulsar.PulsarProperties.Transaction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import com.cestec.cestec.model.corretorDTO;
