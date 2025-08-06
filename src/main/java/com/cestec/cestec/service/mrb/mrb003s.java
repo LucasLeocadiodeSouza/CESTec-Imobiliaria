d485b9304c5cf07caab9d9f983a5bae4d16f7309
Lucas Leocadio de Souza <vanildo1511@gmail.com>
2025-08-06 20:14:05 -0300

    Acabado metodo de salvamento para cadastro de usuario

--- src/main/java/com/cestec/cestec/service/mrb/mrb003s.java
+++ src/main/java/com/cestec/cestec/service/mrb/mrb003s.java
@@ -5,14 +5,18 @@ import java.time.LocalDate;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import com.cestec.cestec.model.cargo;
 import com.cestec.cestec.model.funcionario;
 import com.cestec.cestec.model.funcionarioDTO;
 import com.cestec.cestec.model.pcp_setor;
+import com.cestec.cestec.model.securityLogin.sp_user;
+import com.cestec.cestec.model.securityLogin.userRole;
 import com.cestec.cestec.repository.cargoRepository;
 import com.cestec.cestec.repository.setorRepository;
+import com.cestec.cestec.repository.userRepository;
 import com.cestec.cestec.repository.custom.prjControleDeUsuarioRepository;
 import com.cestec.cestec.repository.generico.funcionarioRepository;
 import com.cestec.cestec.service.sp_userService;
@@ -27,7 +31,10 @@ public class mrb003s {
     private prjControleDeUsuarioRepository prjControleDeUsuarioRepository;
 
     @Autowired
-    private sp_userService sp_user;
+    private sp_userService userService;
+
+    @Autowired
+    private userRepository userRepository;
 
     @Autowired
     private setorRepository setorRepository;
@@ -48,41 +55,67 @@ public class mrb003s {
     @Transactional(rollbackFor = Exception.class)
     public ResponseEntity<?> cadastrarUsuario(funcionarioDTO funcionario, String ideusu, String acao){
         try{
-            if(sp_user.loadUserByUsername(ideusu) == null) return ResponseEntity.ok("Usuário não encontrado no sistema!");
+            if(userService.loadUserByUsername(ideusu) == null) return ResponseEntity.ok("Usuário não encontrado no sistema!");
 
             if(!acao.equals("Inserindo") && 
                 !acao.equals("Alterando")) return ResponseEntity.badRequest().body("Ação informada invalida!");
 
-            if(funcionario.getNomefunc() == null && funcionario.getNomefunc() == "") return ResponseEntity.badRequest().body("Deve ser informado o Nome do Usuario!");
+            if(funcionario.getNomefunc() == null && funcionario.getNomefunc() == "") return ResponseEntity.badRequest().body("Deve ser informado o Nome do Funcionario!");
 
-            if(funcionario.getCpf() == null && funcionario.getCpf() == "") return ResponseEntity.badRequest().body("Deve ser informado o numero do Documento 'cpf' do usuário!");
-            if(funcionario.getCpf().length() != 11) return ResponseEntity.badRequest().body("Deve ser informado o numero do Documento 'cpf' do usuário!");
+            if(funcionario.getCpf() == null && funcionario.getCpf() == "") return ResponseEntity.badRequest().body("Deve ser informado o numero do Documento 'cpf' do Funcionario!");
+            if(funcionario.getCpf().length() != 11) return ResponseEntity.badRequest().body("Deve ser informado o numero do Documento 'cpf' do Funcionario!");
 
-            if(funcionario.getEndereco() == null && funcionario.getEndereco() == "") return ResponseEntity.badRequest().body("Deve ser informado o endereço do usuário!");
+            if(funcionario.getEndereco() == null && funcionario.getEndereco() == "") return ResponseEntity.badRequest().body("Deve ser informado o endereço do Funcionario!");
 
-            if(funcionario.getDatinasc() == null) return ResponseEntity.badRequest().body("Deve ser informado a data de nascimento do usuário!");
+            if(funcionario.getDatinasc() == null) return ResponseEntity.badRequest().body("Deve ser informado a data de nascimento do Funcionario!");
 
-            if(funcionario.getCodsetor() == null) return ResponseEntity.badRequest().body("Deve ser informado o Setor do usuário!");
+            if(funcionario.getCodsetor() == null) return ResponseEntity.badRequest().body("Deve ser informado o Setor do Funcionario!");
             pcp_setor setorfunc = setorRepository.findSetorByCodSetor(funcionario.getCodsetor());
             if(setorfunc == null) return ResponseEntity.badRequest().body("Setor informado não cadastrado!");
 
-            if(funcionario.getCodcargo() == null) return ResponseEntity.badRequest().body("Deve ser informado o Cargo do usuário!");
+            if(funcionario.getCodcargo() == null) return ResponseEntity.badRequest().body("Deve ser informado o Cargo do Funcionario!");
             cargo cargofunc = cargoRepository.findCargoByCodCargo(funcionario.getCodcargo());
             if(cargofunc == null) return ResponseEntity.badRequest().body("Cargo informado não cadastrado!");
 
-            if(funcionario.getSalario() == 0) return ResponseEntity.badRequest().body("Deve ser informado o Salario do usuário!");
+            if(funcionario.getSalario() == 0) return ResponseEntity.badRequest().body("Deve ser informado o Salario do Funcionario!");
 
             funcionario funcAnalise = funcionarioRepo.findFuncBycodfunc(funcionario.getCodfunc());
 
             if(acao.equals("Inserindo")){
-                if(funcAnalise != null) return ResponseEntity.badRequest().body("Usuário [" + funcionario.getNomefunc() + "] já está cadastrado no sistema!");
+                if(funcAnalise != null) return ResponseEntity.badRequest().body("Funcionario [" + funcionario.getNomefunc() + "] já está cadastrado no sistema!");
 
                 funcAnalise = new funcionario();
                 funcAnalise.setIdeusu(ideusu);
                 funcAnalise.setCriado_em(LocalDate.now());
+
+                String loginName = "";
+                String[] nameCompl = funcionario.getNomefunc().split(" ");
+                for (int i = 0; i < nameCompl.length || i < 2; i++) {
+                    String sobrenome = nameCompl[i];
+
+                    if(i == 0){
+                        loginName = nameCompl[0].length() < 9?nameCompl[0] : nameCompl[0].substring(0, 9);
+                    }else{
+                        if(sobrenome.equals("de") || sobrenome.equals("da") || sobrenome.equals("do")) continue;
+                        if(loginName.length() >= 15) break;
+
+                        loginName += sobrenome.substring(0, 1);
+                    }
+                }
+
+                sp_user usuarioAnalise = userService.getUserByIdeusu(loginName);
+                if(usuarioAnalise != null) return ResponseEntity.badRequest().body("Usuário [" + funcionario.getNomefunc() + "] já está cadastrado no sistema! 1");
+                else usuarioAnalise = new sp_user();
+
+                usuarioAnalise.setLogin(loginName.toUpperCase());
+                usuarioAnalise.setPasskey(new BCryptPasswordEncoder().encode("1234567"));
+                usuarioAnalise.setRole(userRole.USER);
+                userRepository.save(usuarioAnalise);
+
+                funcAnalise.setSp_user(usuarioAnalise);
             }
             else if(acao.equals("Alterando")){
-                if(funcAnalise == null) return ResponseEntity.badRequest().body("Usuário [" + funcionario.getNomefunc() + "] não encontrado no sistema sistema!");
+                if(funcAnalise == null) return ResponseEntity.badRequest().body("Funcionario [" + funcionario.getNomefunc() + "] não encontrado no sistema sistema! 2");
             }
 
             funcAnalise.setNome(funcionario.getNomefunc());
@@ -96,7 +129,7 @@ public class mrb003s {
             funcionarioRepo.save(funcAnalise);
             return ResponseEntity.ok("OK");
         }catch (Exception e) {
-            return ResponseEntity.internalServerError().body("Erro ao cadastrar o Usuário: " + e.getMessage());    
+            return ResponseEntity.internalServerError().body("Erro ao cadastrar o Funcionario: " + e.getMessage());    
         }
     }
 
