8f57c61fc6a096291ab227c16e3addd9b5a0d9ac
Lucas Leocadio de Souza <vanildo1511@gmail.com>
2025-08-22 23:55:36 -0300

    Criado funcoes para salvar solicitacoes e direcionar para usuarios do sistema

--- src/main/java/com/cestec/cestec/repository/opr/opr002Repo.java
+++ src/main/java/com/cestec/cestec/repository/opr/opr002Repo.java
@@ -18,11 +18,12 @@ public class opr002Repo {
     }
 
     
-    public List<chamadoSolicDTO> buscarChamados(String ideusu, boolean somenteAtivo){
+    public List<chamadoSolicDTO> buscarSolicitacoes(String ideusu, boolean somenteAtivo){
         String query   = "SELECT new com.cestec.cestec.model.opr.chamadoSolicDTO(" + 
                          "solic.id, func.nome, solic.titulo, solic.descricao, solic.estado, solic.prioridade, solic.complex, solic.previsao, solic.obs, solic.datconcl, solic.feedback, solic.datregistro, solic.ideususolic)" + 
                          " FROM opr_chamados_solic solic " +
                          " JOIN solic.codfuncfila func";
+
         boolean temand = false;
 
         if(ideusu != null){
@@ -30,17 +31,46 @@ public class opr002Repo {
             temand = true;
         }
         if(somenteAtivo){
-            query += (temand?" AND ":" WHERE ") + " solic.estado = :3 ";
+            query += (temand?" AND ":" WHERE ") + " solic.estado = 1 OR solic.estado = 2 ";
             temand = true;
         }  
+
 
         var q = em.createQuery(query, chamadoSolicDTO.class);
 
         if(ideusu != null){
             q.setParameter("ideusu", ideusu);
         }
+
+        return q.getResultList();
+    }
+
+    public List<chamadoSolicDTO> buscarChamados(String ideusu, boolean somenteAtivo, String acao){
+        String query   = "SELECT new com.cestec.cestec.model.opr.chamadoSolicDTO(" + 
+                         "solic.id, funcfila.nome, solic.titulo, solic.descricao, solic.estado, solic.prioridade, solic.complex, solic.previsao, solic.obs, solic.datconcl, solic.feedback, solic.datregistro, solic.ideususolic)" + 
+                         " FROM opr_chamados_solic solic " +
+                         " JOIN solic.codfuncfila funcfila" +
+                         " JOIN funcfila.sp_user user" +
+                         " WHERE solic.estado = 1";
+        boolean temand = false;
+
+        if(ideusu != null){
+            query += " AND user.login LIKE :ideusu ";
+            temand = true;
+        }
         if(somenteAtivo){
-            q.setParameter("estado", 3);
+            query += " AND  solic.estado = 3 ";
+            temand = true;
+        }  
+        if(acao.equals("direcionar")){
+            query += " AND  user.login LIKE 'FILA' ";
+            temand = true;
+        }
+
+        var q = em.createQuery(query, chamadoSolicDTO.class);
+
+        if(ideusu != null){
+            q.setParameter("ideusu", ideusu);
         }
 
         return q.getResultList();
--- src/main/java/com/cestec/cestec/service/opr/opr002s.java
+++ src/main/java/com/cestec/cestec/service/opr/opr002s.java
@@ -3,13 +3,19 @@ package com.cestec.cestec.service.opr;
 import java.sql.Date;
 import java.time.LocalDate;
 import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import com.cestec.cestec.model.funcionario;
+import com.cestec.cestec.model.modelUtilForm;
 import com.cestec.cestec.model.opr.chamadoSolicDTO;
+import com.cestec.cestec.model.opr.opr_chamados_solic;
 import com.cestec.cestec.repository.generico.funcionarioRepository;
 import com.cestec.cestec.repository.opr.chamadosSolicRepo;
 import com.cestec.cestec.repository.opr.opr002Repo;
+import com.cestec.cestec.service.genService;
+import com.cestec.cestec.service.sp_userService;
 import com.cestec.cestec.util.utilForm;
 
 @Service
@@ -22,6 +28,12 @@ public class opr002s {
 
     @Autowired
     private opr002Repo opr002repo;
+
+    @Autowired
+    private sp_userService sp_user;
+
+    @Autowired
+    private genService gen;
 
     /* ********** Funcoes ********** */
 
@@ -36,6 +48,7 @@ public class opr002s {
 
     public String getEstado(Integer codestado){
         switch (codestado) {
+        case 0: return "Aguardando Env.";
         case 1: return "Direcionado";
         case 2: return "Iniciado";
         case 3: return "Finalizado";
@@ -55,12 +68,123 @@ public class opr002s {
         return "";
     }
 
+    public List<modelUtilForm> getOptionsPrioridade(){
+        List<modelUtilForm> listaMotivos = new java.util.ArrayList<>();
+        int i = 1;
+
+        listaMotivos.add(new modelUtilForm(0, "Selecione a Prioridade"));
+
+        do{
+            listaMotivos.add(new modelUtilForm(i, getPrioridade(i)));
+            i++;
+        }while (getPrioridade(i) != "");
+        
+        return listaMotivos;
+    }
+
+    public String getNomeUsuario(String ideusu){
+        return gen.getNomeByIdeusu(ideusu);
+    }
+
+
+    @Transactional(rollbackFor = Exception.class)
+    public ResponseEntity<?> abrirSolicitacao(chamadoSolicDTO solicitacao){
+        try{
+            if(sp_user.loadUserByUsername(solicitacao.getIdeususolic()) == null) return ResponseEntity.badRequest().body("Usuário não encontrado no sistema!");
+
+            opr_chamados_solic solicanalise = chamadosSolicRepo.findByIdSolic(solicitacao.getIdsolic());
+            if (solicanalise == null) solicanalise = new opr_chamados_solic();
+
+            if(solicitacao.getTitulo() == null || solicitacao.getTitulo() == "") return ResponseEntity.badRequest().body("Deve ser informado um titulo para a solicitacao");
+
+            if(solicitacao.getDescricao() == null || solicitacao.getDescricao() == "")  return ResponseEntity.badRequest().body("Deve ser informado uma Descricão para cadastrar a solicitacão");
+
+            if(solicitacao.getPrioridade() == null || solicitacao.getPrioridade() == 0)  return ResponseEntity.badRequest().body("Deve ser informado uma prioridade para cadastrar a solicitacao");
+            if(getPrioridade(solicitacao.getPrioridade()) == "") return ResponseEntity.badRequest().body("Prioridade da solicitacão invalido!");
+
+            solicanalise.setTitulo(solicitacao.getTitulo());
+            solicanalise.setDescricao(solicitacao.getDescricao());
+            solicanalise.setPrioridade(solicitacao.getPrioridade());
+
+            funcionario funcFila = funcionarioRepo.findFuncBycodfunc(6);
+            solicanalise.setCodfuncfila(funcFila);
+
+            if(solicitacao.getIdsolic() == null || solicitacao.getIdsolic() == 0){
+                solicanalise.setEstado(0);
+                solicanalise.setComplex(0);
+                solicanalise.setDatregistro(LocalDate.now());
+                solicanalise.setIdeususolic(solicitacao.getIdeususolic());
+            }
+
+            chamadosSolicRepo.save(solicanalise);
+            return ResponseEntity.ok("OK");
+        }catch (Exception e) {
+            return ResponseEntity.internalServerError().body("Erro ao cadastrar o solicitacao: " + e.getMessage());    
+        }
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    public ResponseEntity<?> enviarSolicitacao(String ideusu, Integer idsolic){
+        try{
+            if(sp_user.loadUserByUsername(ideusu) == null) return ResponseEntity.badRequest().body("Usuário não encontrado no sistema!");
+
+            opr_chamados_solic solicanalise = chamadosSolicRepo.findByIdSolic(idsolic);
+            if (solicanalise == null) return ResponseEntity.badRequest().body("Não encontrado a Solicitacão com o codigo '" + idsolic + "'");
+
+            solicanalise.setEstado(1);
+
+            chamadosSolicRepo.save(solicanalise);
+            return ResponseEntity.ok("OK");
+        }catch (Exception e) {
+            return ResponseEntity.internalServerError().body("Erro ao Enviar a solicitacao para fila: " + e.getMessage());    
+        }
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    public ResponseEntity<?> direcionarSolic(String ideusu, String ideusudirec, Integer idsolic){
+        try{
+            if(sp_user.loadUserByUsername(ideusu) == null) return ResponseEntity.badRequest().body("Usuário não encontrado no sistema!");
+
+            opr_chamados_solic solicanalise = chamadosSolicRepo.findByIdSolic(idsolic);
+            if (solicanalise == null) return ResponseEntity.badRequest().body("Não encontrado a Solicitacão com o codigo '" + idsolic + "'");
+
+            funcionario func = funcionarioRepo.findFuncByIdeusu(ideusudirec);
+            solicanalise.setCodfuncfila(func);
+
+            chamadosSolicRepo.save(solicanalise);
+            return ResponseEntity.ok("OK");
+        }catch (Exception e) {
+            return ResponseEntity.internalServerError().body("Erro ao Direcionar a solicitacao: " + e.getMessage());    
+        }
+    }
+
+    @Transactional(rollbackFor = Exception.class)
+    public ResponseEntity<?> finalizarSolicitacao(String ideusu, String obs, Integer idsolic){
+        try{
+            if(sp_user.loadUserByUsername(ideusu) == null) return ResponseEntity.badRequest().body("Usuário não encontrado no sistema!");
+
+            if(obs == null || obs.isBlank()) return ResponseEntity.badRequest().body("Deve ser fornecido uma observacão para Finalizar a Solicitacão!");
+            
+            opr_chamados_solic solicanalise = chamadosSolicRepo.findByIdSolic(idsolic);
+            if (solicanalise == null) return ResponseEntity.badRequest().body("Não encontrado a Solicitacão com o codigo '" + idsolic + "'");
+                
+            solicanalise.setEstado(4);
+            solicanalise.setObs(obs);
+            solicanalise.setDatconcl(LocalDate.now());
+
+            chamadosSolicRepo.save(solicanalise);
+            return ResponseEntity.ok("OK");
+        }catch (Exception e) {
+            return ResponseEntity.internalServerError().body("Erro ao Finalizar a solicitacao: " + e.getMessage());    
+        }
+    }
+
     /* ********** GRIDS ********** */
 
-    public List<?> carregarGridChamados(String ideusu, Integer somenteAtivo){
+    public List<?> carregarGridChamados(String ideusu, Integer somenteAtivo, String acao){
         boolean vsomenteAtivo = somenteAtivo == 1;
 
-        List<chamadoSolicDTO> solic = opr002repo.buscarChamados(ideusu, vsomenteAtivo);
+        List<chamadoSolicDTO> solic = opr002repo.buscarChamados(ideusu, vsomenteAtivo, acao);
 
         utilForm.initGrid();
         for (int i = 0; i < solic.size(); i++) {
@@ -88,6 +212,33 @@ public class opr002s {
             utilForm.criarColuna(solic.get(i).getFeedback());
             utilForm.criarColuna(utilForm.formatarDataBrasil(dataPrev));
             utilForm.criarColuna(solic.get(i).getObs());
+            utilForm.criarColuna(solic.get(i).getIdeusufila());
+        }
+
+        return utilForm.criarGrid();
+    }
+
+    public List<?> carregarGridChamSolicitados(String ideusu, Integer somenteAtivo){
+        boolean vsomenteAtivo = somenteAtivo == 1;
+
+        List<chamadoSolicDTO> solic = opr002repo.buscarSolicitacoes(ideusu, vsomenteAtivo);
+
+        utilForm.initGrid();
+        for (int i = 0; i < solic.size(); i++) {
+            Date dataCriacao = Date.valueOf(LocalDate.now());
+            
+            if(solic.get(i).getDatregistro() != null) dataCriacao = Date.valueOf(solic.get(i).getDatregistro());
+
+            utilForm.criarRow();
+            utilForm.criarColuna(solic.get(i).getIdsolic().toString());
+            utilForm.criarColuna(solic.get(i).getTitulo()); 
+            utilForm.criarColuna(utilForm.formatarDataBrasil(dataCriacao));
+            utilForm.criarColuna(getEstado(solic.get(i).getEstado()));
+            utilForm.criarColuna(getPrioridade(solic.get(i).getPrioridade()));
+            utilForm.criarColuna(solic.get(i).getIdeususolic());
+            utilForm.criarColuna(solic.get(i).getDescricao());
+            utilForm.criarColuna(solic.get(i).getPrioridade().toString());
+            utilForm.criarColuna(solic.get(i).getEstado().toString());
             utilForm.criarColuna(solic.get(i).getIdeusufila());
         }
 
