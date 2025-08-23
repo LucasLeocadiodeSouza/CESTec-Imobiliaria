b12d6a5bc814476877a1ade5062fa69891f901af
Lucas Leocadio de Souza <vanildo1511@gmail.com>
2025-08-22 23:55:36 -0300

    Criado funcoes para salvar solicitacoes e direcionar para usuarios do sistema

--- src/main/java/com/cestec/cestec/repository/opr/chamadosSolicRepo.java
+++ src/main/java/com/cestec/cestec/repository/opr/chamadosSolicRepo.java
@@ -4,11 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
-
-import com.cestec.cestec.model.opr.opr_agendamentos;
 import com.cestec.cestec.model.opr.opr_chamados_solic;
 
 @Repository
 public interface chamadosSolicRepo extends  JpaRepository<opr_chamados_solic, Integer>{
-
+    @Query("SELECT solic FROM opr_chamados_solic solic WHERE solic.id = :idsolic")
+    opr_chamados_solic findByIdSolic(@Param("idsolic") Integer idsolic);
 }
--- src/main/java/com/cestec/cestec/service/genService.java
+++ src/main/java/com/cestec/cestec/service/genService.java
@@ -1,8 +1,16 @@
 package com.cestec.cestec.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import com.cestec.cestec.repository.generico.funcionarioRepository;
 
 @Service
 public class genService {
-    
+    @Autowired
+    private funcionarioRepository funcionarioRepository;
+
+    public String getNomeByIdeusu(String ideusu){
+        return funcionarioRepository.findNomeByIdeusu(ideusu);
+    }
 }
