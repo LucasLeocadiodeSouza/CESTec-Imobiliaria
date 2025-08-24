1c5240eeb78b852211347facedf0a304938d4c70
Lucas Leocadio de Souza <vanildo1511@gmail.com>
2025-08-24 01:27:04 -0300

    Adicionado metodos para cadastrar programas, removido esquema de horarios e adicionado metodo de finalizacao de solic

--- src/main/java/com/cestec/cestec/repository/opr/opr002Repo.java
+++ src/main/java/com/cestec/cestec/repository/opr/opr002Repo.java
@@ -3,6 +3,9 @@ package com.cestec.cestec.repository.opr;
 import java.util.List;
 import org.springframework.stereotype.Repository;
 import com.cestec.cestec.model.opr.chamadoSolicDTO;
+import com.cestec.cestec.model.opr.opr_versionamento;
+import com.cestec.cestec.model.opr.opr_versionamentoId;
+
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
 
@@ -17,7 +20,11 @@ public class opr002Repo {
         this.em = em;
     }
 
-    
+    public opr_versionamento getVersionamentoById(Integer codmerge, String branchName, String prog, Integer idChamadosSolic) {
+        opr_versionamentoId versionamentoId = new opr_versionamentoId(codmerge, branchName, prog, idChamadosSolic);
+        return em2.find(opr_versionamento.class, versionamentoId);
+    }
+
     public List<chamadoSolicDTO> buscarSolicitacoes(String ideusu, boolean somenteAtivo){
         String query   = "SELECT new com.cestec.cestec.model.opr.chamadoSolicDTO(" + 
                          "solic.id, func.nome, solic.titulo, solic.descricao, solic.estado, solic.prioridade, solic.complex, solic.previsao, solic.obs, solic.datconcl, solic.feedback, solic.datregistro, solic.ideususolic)" + 
@@ -47,21 +54,17 @@ public class opr002Repo {
 
     public List<chamadoSolicDTO> buscarChamados(String ideusu, boolean somenteAtivo, String acao){
         String query   = "SELECT new com.cestec.cestec.model.opr.chamadoSolicDTO(" + 
-                         "solic.id, funcfila.nome, solic.titulo, solic.descricao, solic.estado, solic.prioridade, solic.complex, solic.previsao, solic.obs, solic.datconcl, solic.feedback, solic.datregistro, solic.ideususolic)" + 
+                         "solic.id, funcfila.nome, user.login, solic.titulo, solic.descricao, solic.estado, solic.prioridade, solic.complex, solic.previsao, solic.obs, solic.datconcl, solic.feedback, solic.datregistro, solic.ideususolic)" + 
                          " FROM opr_chamados_solic solic " +
                          " JOIN solic.codfuncfila funcfila" +
                          " JOIN funcfila.sp_user user" +
-                         " WHERE solic.estado = 1";
+                         (!somenteAtivo?" WHERE solic.estado != 0 ":" WHERE solic.estado = 1 ");
         boolean temand = false;
 
         if(ideusu != null){
             query += " AND user.login LIKE :ideusu ";
             temand = true;
         }
-        if(somenteAtivo){
-            query += " AND  solic.estado = 3 ";
-            temand = true;
-        }  
         if(acao.equals("direcionar")){
             query += " AND  user.login LIKE 'FILA' ";
             temand = true;
--- src/main/java/com/cestec/cestec/repository/opr/versionamentoRepo.java
+++ src/main/java/com/cestec/cestec/repository/opr/versionamentoRepo.java
@@ -1,12 +1,12 @@
 package com.cestec.cestec.repository.opr;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
-
 import com.cestec.cestec.model.opr.opr_versionamento;
 import com.cestec.cestec.model.opr.opr_versionamentoId;
 
 @Repository
 public interface versionamentoRepo extends JpaRepository<opr_versionamento, opr_versionamentoId>{
-    
+    List<opr_versionamento> findByChamadoSolicId(Integer idChamadosSolic);
 }
