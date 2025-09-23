dd9d3cffaba9cfb4367246546a34f0d5693c9f62
Lucas Leocadio de Souza <vanildo1511@gmail.com>
2025-09-22 21:49:22 -0300

    Adicionado funcao para salvar imagem e buscar as imagens para os imoveis

--- src/main/java/com/cestec/cestec/repository/cri/imovelImgRepo.java
+++ src/main/java/com/cestec/cestec/repository/cri/imovelImgRepo.java
@@ -10,4 +10,7 @@ import com.cestec.cestec.model.cri.pcp_imovel_imgId;
 public interface imovelImgRepo extends JpaRepository<pcp_imovel_img, pcp_imovel_imgId>{
     @Query("SELECT imagens FROM pcp_imovel_img imagens WHERE imagens.id.codimovel = :codimovel")
     List<pcp_imovel_img> findAllImgByCodimovel(@Param("codimovel") Integer codimovel);
+
+    @Query("SELECT MAX(i.id.seq) FROM pcp_imovel_img i WHERE i.id.codimovel = :codimovel")
+    Integer findMaxSeqByCodimovel(@Param("codimovel") Integer codimovel);
 }
